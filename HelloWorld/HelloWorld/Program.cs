f96d0c290c75a32500a799482e9cc3e2e8f3e2fa
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2021-11-27 16:50:45 +0200

    Section 1
    Environment Set up and Hello World Program.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; // -> using this namespace
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args) // -> Entry Point of program
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Christiaan");
+
+            Console.Read(); // Reads next character, keeps conosole
+        }
+    }
+}
