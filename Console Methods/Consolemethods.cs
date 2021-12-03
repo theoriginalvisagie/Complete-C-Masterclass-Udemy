24875c67eca16ef0cf7c384a92f89c023f7ca4ae
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2021-12-03 11:44:57 +0200

    Links to microsoft docs

--- Console Methods/Consolemethods.cs
+++ Console Methods/Consolemethods.cs
@@ -26,3 +26,6 @@ namespace ConsoleMethods
         }
     }
 }
+
+//Link to Console Methods Docs
+//https://docs.microsoft.com/en-us/dotnet/api/system.console.writeline?view=netcore-3.1#System_Console_WriteLine_System_String_
