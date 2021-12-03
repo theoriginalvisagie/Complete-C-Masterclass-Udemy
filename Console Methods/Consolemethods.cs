3e708621e3f622eee0eb24e399c6f1735547438b
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2021-12-03 11:27:41 +0200

    Console Methods
    Description of basic console commands in C#

---
+++ Console Methods/Consolemethods.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace ConsoleMethods
+{
+    class Program
+    {
+        ///<summary>
+        ///System Methods available and their uses
+        ///</summary>        
+        static void Main(string[] args)
+        {
+            //Prints and keeps cursor on same line.
+            Console.Write("Hello World!");
+
+            //Prints and moves cursor to next line.
+            Console.WriteLine("Hello World!");
+
+            //Takes single inout of type string and returns ASCII value
+            Console.Read();
+
+            //Reads input from user Returns input as Output
+            Console.ReadLine();
+
+            //Takes input of type string and returns the key info
+            Console.ReadKey();
+        }
+    }
+}
