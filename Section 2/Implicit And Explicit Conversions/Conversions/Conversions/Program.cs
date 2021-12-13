e2f24d890589728d566f44d43174ac862332b55b
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2021-12-13 18:57:33 +0200

    DIR sorting And Conversions
    Implicit and Explicit Conversions covered

---
+++ Section 2/Implicit And Explicit Conversions/Conversions/Conversions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Conversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Implicit Conversion
+            //Big to small 
+            int num = 123456789;
+            long bigNum = num;
+
+            //Explicit Conversion
+            double myDouble = 13.75;
+            int myInt;
+            
+            //Cast double to int
+            myInt = (int)myDouble;
+            Console.WriteLine(myInt);
+            Console.Read();
+
+            //type Conversion
+            string myString = myDouble.ToString();
+            num.ToString();
+
+            bool sunIsShining = false;
+            string myBoolString = sunIsShining.ToString();
+
+            Console.WriteLine(myBoolString);
+            Console.Read();
+        }
+    }
+}
