dc255ecaf5a6ac056fcdf75ca1777e336b3adacc
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2021-12-13 19:03:09 +0200

    Parsing
    Converting strings to Ints

---
+++ Section 2/Implicit And Explicit Conversions/Parsing/Parsing/Parsing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Parsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "15";
+            string mySecondString = "13";
+
+            string result = myString + mySecondString;
+
+            //Convert string to int
+            int num1 = Int32.Parse(myString);
+            int num2 = Int32.Parse(mySecondString);
+            int resultTwo = num1 + num2;
+
+            //1513
+            Console.WriteLine(result);
+            //28
+            Console.WriteLine(resultTwo);
+
+            Console.Read();
+        }
+    }
+}
