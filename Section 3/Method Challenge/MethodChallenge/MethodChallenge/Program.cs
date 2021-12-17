68639fcf0ce9d594bd8324770feccd201a855330
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2021-12-17 18:59:47 +0200

    Methods Challenge

---
+++ Section 3/Method Challenge/MethodChallenge/MethodChallenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*
+ *Challenge:
+ *Create GreetFriend Method that writes onto console when called
+ *Name in greeting needs to be replaced with argument
+ */
+namespace MethodChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GreetFriend("Nathan");
+            GreetFriend("Dylan");
+            GreetFriend("Schalk");
+        }
+
+        public static void GreetFriend(string myFriendName)
+        {
+            Console.WriteLine($"Hi {myFriendName}, how are you?");
+        }
+    }
+}
