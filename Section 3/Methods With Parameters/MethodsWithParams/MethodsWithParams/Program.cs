decdac0bddc61d16e5db5685d63b534e83969f8a
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2021-12-17 18:31:11 +0200

    Methods with Parameters
    Basic maths functions with in different methods

---
+++ Section 3/Methods With Parameters/MethodsWithParams/MethodsWithParams/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MethodsWithParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Add(Add(1, 2), Add(3, 4));
+            Console.WriteLine(Add(15, 31));
+            Console.WriteLine(result);
+            Console.WriteLine(Multiply(5,5));
+            Console.WriteLine(Divide(25, 5));
+
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Divide(double d1, double d2)
+        {
+            return d1 / d2;
+        }
+    }
+
+}
