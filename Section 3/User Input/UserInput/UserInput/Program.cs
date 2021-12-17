71c58f0b4d968b42324f8305e29dc7ebea80aab3
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2021-12-17 19:06:04 +0200

    User Input
    Method to take in two user inputs and calculate the sum of them

---
+++ Section 3/User Input/UserInput/UserInput/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculate());
+            Console.Read();
+        }
+
+        public static int Calculate()
+        {
+            Console.WriteLine("Please enter the first number.");
+            string num1Input = Console.ReadLine();
+
+            Console.WriteLine("Please enter the second number.");
+            string num2Input = Console.ReadLine();
+
+            int num1 = int.Parse(num1Input);
+            int num2 = int.Parse(num2Input);
+            int result = num1 + num2;
+
+            return result;
+        }
+    }
+}
