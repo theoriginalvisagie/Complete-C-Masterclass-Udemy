09626aff400c2065f1af14783422c445fa479d33
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-01-04 10:14:53 +0200

    If statements and Else If

---
+++ Section 4/If Statments/IfStatment/IfStatment/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace IfStatment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's teh temprature like?");
+            string temprature = Console.ReadLine();
+            int numTemp = int.Parse(temprature);
+
+            if (numTemp < 20)
+            {
+                Console.WriteLine("Take your coat");
+            }
+            else if (numTemp == 20)
+            {
+                Console.WriteLine("It's 10 degrees outside");
+            }
+            else
+            {
+                Console.WriteLine("Shorts are enought today");
+            }
+
+            //Ineffeciant code
+            //runs through all conditions even if one is met
+
+            /*if (numTemp < 20)
+            {
+                Console.WriteLine("Take your coat");
+            }
+
+            if (numTemp == 20)
+            {
+                Console.WriteLine("It's 10 degrees outside");
+            }
+
+            if (numTemp > 20)
+            {
+                Console.WriteLine("Shorts are enought today");
+            }*/
+
+            Console.Read();
+        }
+    }
+}
