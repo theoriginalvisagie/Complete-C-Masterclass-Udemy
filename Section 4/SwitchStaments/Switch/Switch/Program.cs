9811142902607b6b1056cc0b22bfaaab9551e7e3
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-01-11 06:44:22 +0200

    Switch Statements

---
+++ Section 4/SwitchStaments/Switch/Switch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 25;
+
+            switch (age)
+            {
+                case 15:
+                    Console.WriteLine("Too young");
+                    break;
+                case 25:
+                    Console.WriteLine("YOOOO");
+                    break;
+                case 35:
+                    Console.WriteLine("Very Old");
+                    break;
+                default:
+                    Console.WriteLine("How Old are you?");
+                    break;
+
+            }
+        }
+    }
+}
