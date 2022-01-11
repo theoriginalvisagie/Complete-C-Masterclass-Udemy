1aabef86183e785558369d7fbc3b879f375fb65c
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-01-11 07:05:42 +0200

    Ternary Operator Challenge

---
+++ Section 4/Ternary Operator Challenge/Temparature/Temparature/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Temparature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputTemprature = 0;
+            string tempMessage = string.Empty;
+            string inputValue = string.Empty;
+
+            Console.WriteLine("Enter the current temprature : ");
+            inputValue = Console.ReadLine();
+
+            bool validInteger = int.TryParse(inputValue, out inputTemprature);
+
+            if (validInteger)
+            {
+                tempMessage = inputTemprature <= 15 ? "It is cold" : (inputTemprature >= 16 && inputTemprature <= 28) ? "It is hot" : "";
+
+                Console.WriteLine(tempMessage);
+            }
+            else {
+                Console.WriteLine("Not a valid temprature");
+            }
+        }
+    }
+}
