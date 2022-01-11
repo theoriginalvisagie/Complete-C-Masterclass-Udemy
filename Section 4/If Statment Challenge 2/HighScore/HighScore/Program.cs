eede152194bc3b752ac1e984858f79a89c893fd2
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-01-11 06:50:55 +0200

    IF statement challenge 2
    Check if highs core has been broken

---
+++ Section 4/If Statment Challenge 2/HighScore/HighScore/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HighScore
+{
+    class Program
+    {
+        static int highScore = 300;
+        static string highScorePlayer = "Denis";
+        static void Main(string[] args)
+        {
+            CheckHighScore(250, "Maria");
+            CheckHighScore(350, "Neckers");
+
+            Console.Read();
+        }
+
+        public static void CheckHighScore(int score, string playerName)
+        {
+            if (highScore < score)
+            {
+                highScore = score;
+                highScorePlayer = playerName;
+                Console.WriteLine($"New highscore is {score}");
+                Console.WriteLine($"Highscore held by {playerName}");
+            }
+            else {
+                Console.WriteLine($"Highscore {highScore} from {highScorePlayer} still stands!");
+            }
+        }
+    }
+}
