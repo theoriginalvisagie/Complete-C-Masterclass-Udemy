b476891e2f4a9899d866460fa3044dc619fde537
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-01-11 06:58:42 +0200

    Ternary Operator

---
+++ Section 4/Ternary Operator/TernaryOperator/TernaryOperator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TernaryOperator
+{
+    class Program
+    {
+        //Simplified version of is statment
+        static void Main(string[] args)
+        {
+            int temprature = -5;
+            string stateOfMatter;
+
+            stateOfMatter = temprature < 0 ? "Solid" : "Liquid";
+
+            Console.WriteLine($"State of matter is {stateOfMatter}");
+
+            temprature += 30;
+            stateOfMatter = temprature < 0 ? "Solid" : "Liquid";
+            Console.WriteLine($"State of matter is {stateOfMatter}");
+
+            temprature += 110;
+            stateOfMatter = temprature > 100 ? "Gas" : temprature < 0 ? "Solid" : "Liquid";
+            Console.WriteLine($"State of matter is {stateOfMatter}");
+            Console.ReadKey();
+        }
+    }
+}
