aa44fea981a3d76eefd76fb331a0af2cc889db69
Christiaan Visagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-01-25 19:21:32 +0200

    For Loops

---
+++ Section 5/For Loops/ForLoops/ForLoops/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Increments by 1 untill 9 is reached
+            for (int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine(counter);
+            }
+
+            //increments by 5 until smaller than 50 reached
+            for (int counter = 0; counter < 50; counter += 5)
+            {
+                Console.WriteLine(counter);
+            }
+            
+            //prints out odd numbers between 0 and 20
+            for (int counter = 0; counter <=20; counter++)
+            {
+                if (counter % 2 != 0) 
+                {
+                    Console.WriteLine(counter);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
