dca75b1729f3dc399fc921c225f4900a40d7dd50
Christiaan Visagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-01-25 20:07:42 +0200

    Break And Continue

---
+++ Section 5/Break And Continue/BreakAndContinue/BreakAndContinue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BreakAndContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine(counter);
+                if (counter == 3)
+                {
+                    Console.WriteLine("At 3 we Stop");
+                    break;
+                }
+               
+            }
+
+            for (int counter = 0; counter < 10; counter++)
+            {
+                
+                if (counter == 3)
+                {
+                    Console.WriteLine("We skip 3");
+                    continue;
+                }
+                Console.WriteLine(counter);
+            }
+            Console.Read();
+        }
+    }
+}
