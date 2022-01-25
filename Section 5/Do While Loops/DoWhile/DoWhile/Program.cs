614669807495eed43ffa679abc4f03b358f58dcb
Christiaan Visagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-01-25 19:29:25 +0200

    Do While Loops

---
+++ Section 5/Do While Loops/DoWhile/DoWhile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int counter = 15;
+
+            //do
+            //{
+                //Console.WriteLine(counter);
+               // counter++;
+            //} while (counter < 15);
+
+            int lenghtOfText = 0;
+            string wholeText = "";
+            do
+            {
+                Console.WriteLine("Enter name of a friend.");
+                string nameOfFriend = Console.ReadLine();
+                int currentLenght = nameOfFriend.Length;
+                lenghtOfText += currentLenght;
+                wholeText += nameOfFriend;
+            } while (lenghtOfText < 20);
+
+            Console.WriteLine($"Thanks, that's enough {wholeText}" );
+            Console.Read();
+
+        }
+    }
+}
