1a26210c8af13fd15b25111d9db43deb97f4eddf
Christiaan Visagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-01-25 20:01:16 +0200

    While Loop

---
+++ Section 5/While Loops/WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+
+
+            int counter2 = 0;
+            string enteredText = "";
+            while (enteredText.Equals(""))
+            {
+                Console.WriteLine("Hit enter to increase by one " + " +enter if you want to finish" );
+                enteredText = Console.ReadLine();
+                counter2++;
+                Console.WriteLine($"Current people count {counter2}");
+            }
+            Console.WriteLine($"{counter2} people in the bus. Press enter to exit");
+            Console.Read();
+        }
+    }
+}
