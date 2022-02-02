db70c26cbe73f38aefbff858336ddd0820c19be5
Christiaan Visagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-02-02 20:27:24 +0200

    Intro to OOP

---
+++ Section 6 - OOP/Classes/Classes/Classes/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Human
+    {
+        // Member Variable
+        // public -> accessable globaly
+        public string firstName;
+        public string lastName;
+
+        public void IntroduceMyself()
+        {
+            Console.WriteLine($"HI, I'm {firstName} {lastName}.");
+        }
+    }
+}
---
+++ Section 6 - OOP/Classes/Classes/Classes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create object of class
+            // An Instance of Human
+            Human me = new Human();
+            // aAccess public variable
+            me.firstName = "Christiaan";
+            me.lastName = "Visagie";
+            // Call Methods from class
+            me.IntroduceMyself();
+
+            Human michael = new Human();
+            michael.firstName = "Micael";
+            michael.lastName = "Stroh";
+            michael.IntroduceMyself();
+
+            Console.ReadKey();
+        }
+    }
+}
