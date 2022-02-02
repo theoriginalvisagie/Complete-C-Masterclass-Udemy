db60c099f910f79676a1212fc74370376b24751e
Christiaan Visagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-02-02 20:46:10 +0200

    Multi Constructos

---
+++ Section 6 - OOP/Multiple Constructors/Constructors/Constructors/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Human
+    {
+        // Variables can be private if contrustor is present.
+        private string firstName;
+        private string lastName;
+        private string eyeColor;
+        private string age;
+
+        // Default Constructor
+        public Human()
+        {
+            Console.WriteLine("Basic constructor called");
+        }
+
+        public Human(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public Human(string firstName, string lastName, string eyeColor)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+        }
+
+        // Create parameterized constructor.
+        public Human(string firstName, string lastName, string eyeColor, string age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
+
+        public void IntroduceMyself()
+        {
+            if (age == "1")
+            {
+                Console.WriteLine($"Hi I'm {firstName} {lastName} my eyecolor is {eyeColor} and I am {age} year old.");
+            }
+            else if (age == null)
+            {
+                Console.WriteLine($"Hi I'm {firstName} {lastName} my eyecolor is {eyeColor}.");
+            } 
+            else if (eyeColor == null && age == null)
+            {
+                Console.WriteLine($"Hi I'm {firstName} {lastName}.");
+            }
+            else
+            {
+                Console.WriteLine($"Hi I'm {firstName} {lastName} my eyecolor is {eyeColor} and I am {age} years old.");
+            }
+            
+        }
+    }
+}
---
+++ Section 6 - OOP/Multiple Constructors/Constructors/Constructors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human me = new Human("Christiaan", "Visagie","green","26");
+            me.IntroduceMyself();
+
+            Human dylan = new Human("Dylan", "Roodt", "brown", "26");
+            dylan.IntroduceMyself();
+
+            Human sam = new Human("Sam", "Monty", "blue", "1");
+            sam.IntroduceMyself();
+
+            Human basic = new Human();
+            basic.IntroduceMyself();
+
+            Human Frank = new Human("Frank", "Meyer", "black");
+            Frank.IntroduceMyself();
+
+            Human nat = new Human("Natalie", "Summers");
+            nat.IntroduceMyself();
+        }
+    }
+}
