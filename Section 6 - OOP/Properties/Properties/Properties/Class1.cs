8d0d3430be938f8e058ee1787ed2963121ec94fc
Christiaan Visagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-02-02 21:09:05 +0200

    Properties

---
+++ Section 6 - OOP/Properties/Properties/Properties/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Box
+    {
+        // Memeber Variables     
+        private int lenght;
+        // Unsafe Code
+        public int height;
+        public int width;
+        public int volume;
+
+        public void SetLength(int newlength)
+        {
+            this.lenght = newlength;
+        }
+
+        public void DisplayInfo()
+        {
+            volume = lenght * width * height;
+            Console.WriteLine($" Lenght is {lenght}, width is {width}, height is {height} and the volume is {volume}");
+        }
+    }
+}
---
+++ Section 6 - OOP/Properties/Properties/Properties/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+
+            // Can't set lenght due to privacy
+            //box.lenght = 4;
+            box.SetLength(4);
+            box.width = 4;
+            box.height = 4;
+
+            box.DisplayInfo();
+        }
+    }
+}
