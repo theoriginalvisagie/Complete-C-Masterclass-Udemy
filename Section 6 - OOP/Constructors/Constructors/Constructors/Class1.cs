c269e515ceaf61786892ed0611e4c807ada65358
Christiaan Visagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-02-02 20:37:02 +0200

    Constructors

---
+++ Section 6 - OOP/Constructors/Constructors/Constructors/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Human
+    {
+        // Variables can be private if contrustor is present.
+        private string firstName;
+        private string lastName;
+        private string eyeColor;
+        private string age;
+
+        // Create constructor.
+        public Human(string firstName, string lastName, string eyeColor, string age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
+
+        public void IntroduceMyself()
+        {
+            if (age == "1")
+            {
+                Console.WriteLine($"Hi I'm {firstName} {lastName} my eyecolor is {eyeColor} and I am {age} year old.");
+            }
+            else 
+            {
+                Console.WriteLine($"Hi I'm {firstName} {lastName} my eyecolor is {eyeColor} and I am {age} years old.");
+            }
+            
+        }
+    }
+}
---
+++ Section 6 - OOP/Constructors/Constructors/Constructors/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human me = new Human("Christiaan", "Visagie","green","26");
+            me.IntroduceMyself();
+
+            Human dylan = new Human("Dylan", "Roodt", "brown", "26");
+            dylan.IntroduceMyself();
+
+            Human sam = new Human("Sam", "Monty", "blue", "1");
+            sam.IntroduceMyself();
+        }
+    }
+}
