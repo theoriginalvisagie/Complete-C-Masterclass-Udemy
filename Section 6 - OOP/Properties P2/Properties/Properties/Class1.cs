a80d83390577ac12a73eecce2052193159621b65
Christiaan Visagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-02-03 18:42:08 +0200

    properties p2

---
+++ Section 6 - OOP/Properties P2/Properties/Properties/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Box
+    {
+        // Memeber Variables     
+        private int lenght;
+        private int height;
+        //public int width;
+        private int volume;
+
+        public int Width { get; set; }
+
+        // Properties
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                if (height < 0)
+                {
+                    height = -value;
+                }
+                else
+                {
+                    height = value;
+                }
+            }
+        }
+
+        public int Volume
+        {
+            get
+            {
+                return this.height * this.lenght * this.Width;
+            }
+        }
+
+        public Box(int length, int width, int height)
+        {
+            this.lenght = lenght;
+            this.height = height;
+            Width = width;
+        }
+
+        public void SetLength(int newlength)
+        {
+            if (lenght < 0)
+            {
+                throw new Exception("Lenght is less than 0");
+            }
+            else 
+            {
+                this.lenght = newlength;
+            }
+           
+        }
+
+        public int GetVolume()
+        {
+            return this.height * this.lenght * this.Width;
+        }
+
+        public int GetLenth() 
+        {
+            return this.lenght;
+        }
+
+        public void DisplayInfo()
+        {
+            //volume = lenght * width * height;
+            Console.WriteLine($" Lenght is {lenght}, width is {Width}, height is {height} and the volume is {Volume}");
+        }
+    }
+}
---
+++ Section 6 - OOP/Properties P2/Properties/Properties/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box(3,5,4);
+
+            //box.SetLength(4);
+            //box.Width = 5;
+            //box.Height = -4;
+            //Console.WriteLine($"The length is {box.GetLenth()}");
+            //Console.WriteLine($"The volume is {box.GetVolume()}");
+            //Console.WriteLine($"The width is {box.Width}");
+            box.DisplayInfo();
+        }
+    }
+}
