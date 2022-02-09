72ccb3bdacc7564d4cd0f36d938223d1935caa40
Christiaan Visagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-02-09 18:49:51 +0200

    arrays

---
+++ Section 8 - Arrays/Arrays 1/Arrays/Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize array and length.
+            int[] grades = new int[5];
+
+            grades[0] = 20;
+            grades[1] = 4;
+            grades[2] = 6;
+            grades[3] = 13;
+            grades[4] = 34;
+
+            Console.WriteLine($"Grade at index 0 {grades[0]}");
+
+            string input = Console.ReadLine();
+            grades[0] = int.Parse(input);
+
+            Console.WriteLine($"Grade at index 0 {grades[0]}");
+            Console.ReadKey();
+
+            int[] gradesOfMathsStudentsA = { 20, 30, 20, 40, 50 };
+
+            int[] gradesOfMathsStudentsB = new int[] { 23, 56, 43, 78, 56 };
+
+            Console.WriteLine($"length of gradesOfMathsStudentsA is  {gradesOfMathsStudentsA.Length}");
+        }
+    }
+}
---
+++ Section 8 - Arrays/Foreach Loops/ForeachLoops/ForeachLoops/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ForeachLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+
+            // Assing index to value
+            for (int i = 0; i < 10; i++)
+            {
+                nums[i] = i;
+            }
+
+            // Read indes j value
+            for (int j = 0; j < nums.Length; j++)
+            {
+                Console.WriteLine($"Element {j} is {j}");
+            }
+
+            // Foreach loop
+            int cnt = 0;
+            foreach (int k in nums)
+            {
+                Console.WriteLine($"Element {cnt} is {k}");
+                cnt++;
+            }
+
+            //Challenge
+            string[] myFriends = { "Sam", "Dylan", "Marc", "Tiaan", "Rouxmon" };
+
+            foreach (string str in myFriends)
+            {
+                Console.WriteLine($"Good Day {str}, how are you?");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
